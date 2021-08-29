a7807540dde434a6256266f7ae55b89b161515d1
akash744 <akashashok12341999@gmail.com>
2021-08-29 17:57:31 +1200

    implemented basic login

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import './App.css';
-import Hero from "./components/Hero/Hero";
-import Navbar from "./components/Navbar/Navbar";
-import Slider from "./components/Slider/Slider";
-import travel_01 from "./assets/travel-01.jpg";
-import travel_02 from "./assets/travel-02.jpg";
-import travel_03 from "./assets/travel-03.jpg";
-import test_1 from "./assets/test_1.jpg";
+import Login from "./pages/Login"
+import Splash from './pages/Splash';
 
 function App() {
 
@@ -18,22 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar navbarLinks={navbarLinks} />
-      <Hero imageSrc={test_1} />
-      <Slider
-        imageSrc={travel_02}
-        title={"Be an explorer."}
-        subtitle={
-          "Our platform offers a wide variety of unique travel locations!"
-        }
-        flipped={false}
-      />
-      <Slider
-        imageSrc={travel_03}
-        title={"Memories for a lifetime."}
-        subtitle={"Your dream vacation is only a few clicks away."}
-        flipped={true}
-      />
+      <Splash></Splash>
     </div>
   );
 }
